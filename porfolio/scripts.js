ea3383e7f1482465312201dfa36e9a63ecf82a71
0xFF00XX <gwynblade313@gmail.com>
2023-03-20 17:46:25 +0000

    css updated

--- porfolio/scripts.js
+++ porfolio/scripts.js
@@ -1,19 +1,10 @@
 function navBarToggle() {
   var x = document.getElementById("mobile-navbar");
+  var element = document.getElementById("nav-icon2");
+  element.classList.toggle("open");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
   }
-}
-
-$(document).ready(function(){
-	$('#nav-icon2').click(function(){
-		$(this).toggleClass('open');
-	});
-});
-
-function navIconRotation() {
-  var element = document.getElementById("nav-icon2");
-  element.classList.toggle("open");
 }
